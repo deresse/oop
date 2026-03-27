d65f926794f25fbfff06f6ed4b4bbd13ac7ebe46
Deresse Demeke <deresseshallo531@gmail.com>
2026-03-27 14:12:14 +0300

    Add files via upload

---
+++ LabClassOne.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labclassone;
+
+/**
+ *
+ * @author hp
+ */
+public class LabClassOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I'm 40 years old!");
+        System.out.println("My monthly salary is "+ 40000);
+        System.out.println("The sun rises in east"+true);
+        System.out.println("one USD is equivalent to"+150+" ETB");
+    }
+    
+}
